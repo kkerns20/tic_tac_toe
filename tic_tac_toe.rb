5472348cd4a1d931c596a947a87840326977e4f4
Kurt Kerns <kurtkerns20@gmail.com>
2022-01-24 13:17:47 -0600

    finished rubocop refactoring with LS

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -73,17 +73,10 @@ end
 
 def detect_winner(brd)
   WINNING_LINES.each do |line|
-    # if brd[line[0]] == PLAYER_MARKER &&
-    #    brd[line[1]] == PLAYER_MARKER &&
-    #    brd[line[2]] == PLAYER_MARKER
-    #   return 'Player'
-    # elsif brd[line[0]] == COMPUTER_MARKER &&
-    #       brd[line[1]] == COMPUTER_MARKER &&
-    #       brd[line[2]] == COMPUTER_MARKER
-    #   return 'Computer'
-    # end
-    if brd.values_at(line[0], line[1], line[2]).count(PLAYER_MARKER)
-      
+    if brd.values_at(*line).count(PLAYER_MARKER) == 3 # splat operator
+      return 'Player'
+    elsif brd.values_at(*line).count(COMPUTER_MARKER) == 3
+      return 'Computer'
     end
   end
   nil
