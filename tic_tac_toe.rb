98c3d6d635f92f30d047e224af9bd980d55d33e0
Kurt Kerns <kurtkerns20@gmail.com>
2022-01-24 13:13:33 -0600

    refactoring logic to make rubocop happy

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -5,17 +5,21 @@ require 'pry'
 INITIAL_MARKER = ' '
 PLAYER_MARKER = 'X'
 COMPUTER_MARKER = 'O'
+WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
+                [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # cols
+                [[1, 5, 9], [3, 5, 7]]              # diagonals
 
 def prompt(msg)
   puts ">> #{msg}"
 end
 
 def clear_screen
-  system('clear')
+  system 'clear'
 end
 
+# rubocop:disable Metrics/AbcSize
 def display_board(brd)
-  system 'clear'
+  clear_screen
   puts "You're a #{PLAYER_MARKER}. Computer is #{COMPUTER_MARKER}."
   puts ""
   puts "     |     |     "
@@ -31,6 +35,7 @@ def display_board(brd)
   puts "     |     |     "
   puts ""
 end
+# rubocop:enable Metrics/AbcSize
 
 def initialize_board
   new_board = {}
@@ -39,7 +44,7 @@ def initialize_board
 end
 
 def empty_squares(brd)
-  brd.keys.select {|num| brd[num] == INITIAL_MARKER}
+  brd.keys.select { |num| brd[num] == INITIAL_MARKER }
 end
 
 def player_places_piece!(brd)
@@ -67,19 +72,18 @@ def someone_won?(brd)
 end
 
 def detect_winner(brd)
-  winning_lines = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
-                  [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # cols
-                  [[1, 5, 9], [3, 5, 7]]              # diagonals
-  
-  winning_lines.each do |line|
-    if brd[line[0]] == PLAYER_MARKER && 
-       brd[line[1]] == PLAYER_MARKER && 
-       brd[line[2]] == PLAYER_MARKER
-       return 'Player'
-    elsif brd[line[0]] == COMPUTER_MARKER && 
-      brd[line[1]] == COMPUTER_MARKER && 
-      brd[line[2]] == COMPUTER_MARKER
-      return 'Computer'
+  WINNING_LINES.each do |line|
+    # if brd[line[0]] == PLAYER_MARKER &&
+    #    brd[line[1]] == PLAYER_MARKER &&
+    #    brd[line[2]] == PLAYER_MARKER
+    #   return 'Player'
+    # elsif brd[line[0]] == COMPUTER_MARKER &&
+    #       brd[line[1]] == COMPUTER_MARKER &&
+    #       brd[line[2]] == COMPUTER_MARKER
+    #   return 'Computer'
+    # end
+    if brd.values_at(line[0], line[1], line[2]).count(PLAYER_MARKER)
+      
     end
   end
   nil
