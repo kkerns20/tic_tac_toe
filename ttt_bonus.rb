efd6048e00e307a4693089dd7613d8f41a23ec53
Kurt Kerns <kurtkerns20@gmail.com>
2022-02-02 11:54:42 -0600

    added display messages and board

--- ttt_bonus.rb
+++ ttt_bonus.rb
@@ -1,150 +1,85 @@
 # ttt_bonus.rb
 # Bonus Features
 
-require 'pry'
+require 'yaml'
+MESSAGES = YAML.load_file('ttt_bonus_messages.yml')
+
+PLAYER = 'Player'
+COMPUTER = 'Computer'
+YES_OR_NO = %w(y n yes no)
 
 INITIAL_MARKER = ' '
 PLAYER_MARKER = 'X'
 COMPUTER_MARKER = 'O'
+
+
 WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
                 [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # cols
                 [[1, 5, 9], [3, 5, 7]]              # diagonals
 
-def prompt(msg)
-  puts ">> #{msg}"
+def prompt_pause(action, info='', punctuation='')
+  puts ">> #{MESSAGES[action]}"
+  sleep(1.5)
 end
 
 def clear_screen
   system 'clear'
 end
 
-# rubocop:disable Metrics/AbcSize
-def display_board(brd)
+def valid_input
+  answer = nil
+
+  loop do
+    answer = gets.chomp.downcase.strip
+    break if YES_OR_NO.include?(answer)
+    prompt_pause(:invalid)
+  end
+
+  answer == 'y' || answer == 'yes' ? 'y' : 'n'
+end
+
+def quit_game
+  display_goodbye
+  exit
+end
+
+def display_welcome
   clear_screen
-  puts "You're a #{PLAYER_MARKER}. Computer is #{COMPUTER_MARKER}."
+  prompt_pause(:welcome)
+  prompt_pause(:goal)
+  prompt_pause(:way_to_win)
+  prompt_pause(:end_goal)
+  prompt_pause(:begin)
+  answer = valid_input
+  quit_game if answer = 'n'
+end
+
+def display_goodbye
+  prompt_pause(:thank_you)
+  prompt_pause(:quote)
+end
+
+# rubocop:disable MEtric/AbcSize, Metrics/MethodLength
+def display_board(brd, score)
+  clear_screen
+  puts "You are #{PLAYER_MARKER}, the Computer is #{COMPUTER_MARKER}."
+  puts <<~MSG
+              -------------------------------------
+              SCORE: PLAYER: [#{score[PLAYER]}] COMPUTER: [#{score[COMPUTER]}]
+              -------------------------------------
+  MSG
   puts ""
   puts "     |     |     "
   puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}  "
   puts "     |     |     "
-  puts "-----|-----|-----"
+  puts "-----+-----+-----"
   puts "     |     |     "
   puts "  #{brd[4]}  |  #{brd[5]}  |  #{brd[6]}  "
   puts "     |     |     "
-  puts "-----|-----|-----"
+  puts "-----+-----+-----"
   puts "     |     |     "
   puts "  #{brd[7]}  |  #{brd[8]}  |  #{brd[9]}  "
   puts "     |     |     "
   puts ""
 end
-# rubocop:enable Metrics/AbcSize
-
-def initialize_board
-  new_board = {}
-  (1..9).each { |num| new_board[num] = INITIAL_MARKER }
-  new_board
-end
-
-def empty_squares(brd)
-  brd.keys.select { |num| brd[num] == INITIAL_MARKER }
-end
-
-def joinor(arr, delimiter = ', ', conjunction = 'or')
-  case arr.size
-  when 0 then ''
-  when 1 then arr.first
-  when 2 then arr.join(" #{conjunction} ")
-  else
-    arr[-1] = "#{conjunction} #{arr.last}"
-    arr.join(delimiter)
-  end
-end
-
-def player_places_piece!(brd)
-  square = ''
-  loop do
-    prompt "Choose a square: #{joinor(empty_squares(brd))}"
-    square = gets.chomp.to_i
-    break if empty_squares(brd).include?(square)
-    prompt "Sorry, That is not a valid choice."
-  end
-  brd[square] = PLAYER_MARKER
-end
-
-def find_at_risk_square(line, board, marker)
-  if board.values_at(*line).count(marker) == 2
-    board.select{|k,v| line.include?(k) && v == INITIAL_MARKER}.keys.first
-  else
-    nil
-end
-
-def computer_places_piece!(brd)
-  square = nil
-
-  # defense first
-  WINNING_LINES.each do |line|
-    square = find_at_risk_square(line, brd, PLAYER_MARKER)
-    break if square
-  end
-
-  # offense
-  if !square
-    WINNING_LINES.each do |line|
-      square = find_at_risk_square(line, brd, COMPUTER_MARKER)
-      break if square
-    end
-  end
-  
-  # just pick a square
-  if !square
-    square = empty_squares(brd).sample
-  end
-  
-  brd[square] = COMPUTER_MARKER
-end
-
-def board_full?(brd)
-  empty_squares(brd).empty?
-end
-
-def someone_won?(brd)
-  !!detect_winner(brd)
-end
-
-def detect_winner(brd)
-  WINNING_LINES.each do |line|
-    if brd.values_at(*line).count(PLAYER_MARKER) == 3 # splat operator
-      return 'Player'
-    elsif brd.values_at(*line).count(COMPUTER_MARKER) == 3
-      return 'Computer'
-    end
-  end
-  nil
-end
-
-loop do
-  board = initialize_board
-
-  loop do
-    display_board(board)
-
-    player_places_piece!(board)
-    break if someone_won?(board) || board_full?(board)
-
-    computer_places_piece!(board)
-    break if someone_won?(board) || board_full?(board)
-  end
-
-  display_board(board)
-
-  if someone_won?(board)
-    prompt "#{detect_winner(board)} won!"
-  else
-    prompt "It's a tie!"
-  end
-
-  prompt "Play again? (y or n)"
-  answer = gets.chomp
-  break unless answer.downcase.start_with?('y')
-end
-
-prompt "Thanks for playing tic-tac-toe! Goodbye."
+# rubocop:enable MEtric/AbcSize, Metrics/MethodLength
